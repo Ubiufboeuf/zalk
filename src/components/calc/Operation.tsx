e83af5ba26b47025db2f66fdd594cf3e426260d2
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-09-14 15:31:35 -0300

    feat: remplaza visualmente los símbolos por operadores

--- src/components/calc/Operation.tsx
+++ src/components/calc/Operation.tsx
@@ -2,9 +2,22 @@ import { calc } from '@/calc'
 import { useCalcStore } from '@/stores/useCalcStore'
 import { useEffect } from 'preact/hooks'
 
+const symbols: Record<string, string> = {
+  '+': '+',
+  '-': '-',
+  '*': '×',
+  '/': '÷'
+}
+
 export function Operation () {
   const operation = useCalcStore((state) => state.operation)
   const setResult = useCalcStore((state) => state.setResult)
+
+  function parseOperation (operation: string | undefined) {
+    if (!operation) return
+    return operation
+      .replace(/\*/g, (c) => symbols[c])
+  }
   
   useEffect(() => {
     if (!operation) return
@@ -16,7 +29,7 @@ export function Operation () {
   return (
     <div class='w-full h-12 mb-10 px-6 flex items-center'>
       <span class='h-full w-full max-w-full text-3xl text-right wrap-anywhere'>
-        {operation}
+        {parseOperation(operation)}
       </span>
     </div>
   )
